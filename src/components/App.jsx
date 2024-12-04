cd9d25a3e599f4c290fb5df38e55b52aa761caf6
RevainIDN <kov.igor94@gmail.com>
2024-12-04 22:38:54 +0200

    Реализован визуальный стиль компонента 'LanguageSelector'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import '../styles/App.css'
+import LanguageSelector from './LanguageSelector'
 
 export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <div className='translator'>
+        <div className='area enteredArea'>
+          <LanguageSelector type={'sourse'} />
+        </div>
+        <div className='area translatedArea'>
+          <LanguageSelector type={'target'} />
+        </div>
+      </div>
     </>
   )
 }
--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,9 +1,17 @@
 import '../styles/LanguageSelector.css'
 
-export default function LanguageSelector() {
+export default function LanguageSelector({ type }) {
 	return (
 		<>
+			<div className='language-selector'>
+				<ul className='language-list'>
+					{type === 'sourse' && <li className='language-item'><button className='language-btn'>Detect Language</button></li>}
+					<li className='language-item'><button className='language-btn active'>English</button></li>
+					<li className='language-item'><button className='language-btn'>French</button></li>
 
+				</ul>
+				{type === 'target' && <button className='reverse-btn'></button>}
+			</div>
 		</>
 	)
 }
